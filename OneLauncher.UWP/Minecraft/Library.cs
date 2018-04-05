ff3ecc661d84bc22d9d03e5d7fb32e4012a7cc6f
MicroOwl <bestowlcity@outlook.com>
2018-04-05 20:58:43 +0800

    Update KMCCC

---
+++ OneLauncher.UWP/Minecraft/Library.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GoodTimeStudio.OneMinecraftLauncher.UWP.Minecraft
+{
+    public class Library
+    {
+        public string Name;
+
+        public string Path;
+
+        public string Url;
+    }
+}
